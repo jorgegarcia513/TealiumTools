27c74f90b0ea71c4e9021a27c14e6b4b8d14a6c9
Jorge Garcia <jorge.garcia@asics.com>
2024-06-26 11:00:07 -0400

    adding jquery and testing

--- profile_exporter.js
+++ profile_exporter.js
@@ -1,34 +1,38 @@
 // Import Axios and Cheerio
-const axios = require('axios');
-const cheerio = require('cheerio');
+// const axios = require('axios');
+// const cheerio = require('cheerio');
+const $ = require('jquery');
 
-const getPostTitles = async () => {
-	try {
-		// Load Reddit
-		const { data } = await axios.get(
-			'https://old.reddit.com/r/programming/'
-		);
+var tags = $('#tags-table .MuiTableRow-root')
+console.log(tags);
 
-		// Parse HTML with Cheerio
-		const $ = cheerio.load(data);
+// const getPostTitles = async () => {
+// 	try {
+// 		// Load Reddit
+// 		const { data } = await axios.get(
+// 			'https://old.reddit.com/r/programming/'
+// 		);
 
-		// Initialise empty data array
-		const postTitles = [];
+// 		// Parse HTML with Cheerio
+// 		const $ = cheerio.load(data);
 
-		// Iterate over all anchor links for the given selector and ....
-		$('div > p.title > a').each((_idx, el) => {
-			// .... extract for each the tag text and add it to the data array
-			const postTitle = $(el).text()
-			postTitles.push(postTitle)
-		});
+// 		// Initialise empty data array
+// 		const postTitles = [];
 
-		// Return the array with all titles
-		return postTitles;
-	} catch (error) {
-		throw error;
-	}
-};
+// 		// Iterate over all anchor links for the given selector and ....
+// 		$('div > p.title > a').each((_idx, el) => {
+// 			// .... extract for each the tag text and add it to the data array
+// 			const postTitle = $(el).text()
+// 			postTitles.push(postTitle)
+// 		});
 
-// Wait for the promise to fulfill and print the title array it returned
-getPostTitles()
-    .then((postTitles) => console.log(postTitles));
+// 		// Return the array with all titles
+// 		return postTitles;
+// 	} catch (error) {
+// 		throw error;
+// 	}
+// };
+
+// // Wait for the promise to fulfill and print the title array it returned
+// getPostTitles()
+//     .then((postTitles) => console.log(postTitles));
