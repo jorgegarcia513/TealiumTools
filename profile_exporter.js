36ba9d5f10a169839d520c06a12b16f29502a2b0
Jorge Garcia <jorge.garcia@asics.com>
2024-06-26 11:19:13 -0400

    Querying Page for Tag Rows

--- profile_exporter.js
+++ profile_exporter.js
@@ -1,38 +1,3 @@
-// Import Axios and Cheerio
-// const axios = require('axios');
-// const cheerio = require('cheerio');
-const $ = require('jquery');
 
-var tags = $('#tags-table .MuiTableRow-root')
+var tags = querySelectorAll('#tags-table .MuiTableRow-root');
 console.log(tags);
-
-// const getPostTitles = async () => {
-// 	try {
-// 		// Load Reddit
-// 		const { data } = await axios.get(
-// 			'https://old.reddit.com/r/programming/'
-// 		);
-
-// 		// Parse HTML with Cheerio
-// 		const $ = cheerio.load(data);
-
-// 		// Initialise empty data array
-// 		const postTitles = [];
-
-// 		// Iterate over all anchor links for the given selector and ....
-// 		$('div > p.title > a').each((_idx, el) => {
-// 			// .... extract for each the tag text and add it to the data array
-// 			const postTitle = $(el).text()
-// 			postTitles.push(postTitle)
-// 		});
-
-// 		// Return the array with all titles
-// 		return postTitles;
-// 	} catch (error) {
-// 		throw error;
-// 	}
-// };
-
-// // Wait for the promise to fulfill and print the title array it returned
-// getPostTitles()
-//     .then((postTitles) => console.log(postTitles));
